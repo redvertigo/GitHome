7b9f89ed65e8e7a59afb55b484d779619bd7fb9b
Arkadiy Hvan <tayf2016@gmail.com>
2016-03-28 20:48:28 +0300

    second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        int a = 0;
+        int b = 0;
+
+        System.out.println(a + b);
     }
 }
