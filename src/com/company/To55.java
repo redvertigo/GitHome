6ec913ce90dce8cd36c466e7805ee65f8535c67e
Arkadiy Hvan <tayf2016@gmail.com>
2016-04-02 12:29:55 +0300

    Homework2

---
+++ src/com/company/To55.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class To55 {
+    public static void main(String[] args) {
+
+        int a, counter;
+
+        counter = 0;
+        for (a = 1; a <= 55; a = a + 2) {
+            System.out.println(a);
+            counter++;
+        }
+    }
+}
