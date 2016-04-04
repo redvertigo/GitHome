08f3c7db279395828c67acfe5024aa036c87e05b
Arkadiy Hvan <tayf2016@gmail.com>
2016-04-04 19:03:18 +0300

    Homework3

---
+++ src/com/company/Task3.java
@@ -0,0 +1,15 @@
+package com.company;
+
+
+public class Task3 {
+    public static void main(String[] args) {
+
+        int a, counter;
+
+        counter = 0;
+        for (a = 90; a >= 0; a = a - 5) {
+            System.out.println(a);
+            counter++;
+        }
+    }
+}
